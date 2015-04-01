2e218d0cbcc2d1db6bef9c3bdee34cb71f19cbe9
Stephane Maire <stephanemaire71@gmail.com>
2015-04-01 12:04:06 +0200

    add twitter bootstrap

--- Brocfile.js
+++ Brocfile.js
@@ -3,6 +3,11 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
+app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {destDir: 'fonts'});
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {destDir: 'fonts'});
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {destDir: 'fonts'});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
