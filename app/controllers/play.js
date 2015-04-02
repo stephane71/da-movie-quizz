f38e18270c626b70f1314e7ca433a2e91c36409c
Stephane Maire <stephanemaire71@gmail.com>
2015-04-02 12:58:48 +0200

    get popular movies and show a movie backdrop

--- app/controllers/play.js
+++ app/controllers/play.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export
+default Ember.Controller.extend({
+	url_images: 'http://image.tmdb.org/t/p/w342/',
+
+	current: function() {
+		var movie = this.get('model')[0];
+		return {movie: this.url_images + movie.backdrop_path };
+	}.property('model'),
+
 	actions: {
 		onPlay: function() {
 			this.set('init_game', true);
--- app/routes/play.js
+++ app/routes/play.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export
+default Ember.Route.extend({
+	/* Revoir gestion de l'API */
+	key: '7ea5f490261a949e52930517e1b4657c',
+	url: 'https://api.themoviedb.org/3/',
+
+	model: function() {
+		return Ember.$.getJSON(this.url + 'movie/popular?api_key=' + this.key + '&callback=?')
+			.then(function(movie) {
+				return movie.results.map(function(m) {
+					return {
+						id: m.id,
+						backdrop_path: m.backdrop_path,
+						poster_path: m.poster_path
+					};
+				});
+			});
+	}
 });
