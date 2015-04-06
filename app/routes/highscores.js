f8a40c39121173bb624349403c60389aa28f768a
Stephane Maire <stephanemaire71@gmail.com>
2015-04-06 20:25:29 +0200

    print highscores with localstorage and add delete button

--- app/routes/highscores.js
+++ app/routes/highscores.js
@@ -1,8 +1,20 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-	model: function(){
-//		return this.store.find('highscore').sortBy('score').reverse();
-		return this.store.all('highscore').sortBy('score').reverse();
+export
+default Ember.Route.extend({
+	model: function() {
+		return this.store.find('highscore').then(function(data) {
+			return data.sortBy('score').reverse();
+		});
+	},
+
+	actions: {
+		resetHighscores: function() {
+			this.store.find('highscore').then(function(data) {
+				data.forEach(function(record) {
+					record.destroyRecord();
+				});
+			});
+		}
 	}
 });
