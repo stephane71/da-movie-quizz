525bc7e4e6b98eb3b15f88a357dfdc7363cbee88
Stephane Maire <stephanemaire71@gmail.com>
2015-04-17 14:48:18 +0200

    remove snapcar routes

--- app/initializers/snapcar-api.js
+++
@@ -1,8 +0,0 @@
-export function initialize( container, application ) {
-	application.inject('route', 'snapcar-api', 'service:snapcar-api');
-}
-
-export default {
-  name: 'snapcar-api',
-  initialize: initialize
-};
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	redirect: function(model, transition){
-		//this.transitionTo('play');
-		this.transitionTo('reservation');
+		this.transitionTo('play');
 	}
 });
